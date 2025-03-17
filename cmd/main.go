46761045385dee9acf1e9052b7427ad173d2e30e
spitfire <danil-mizev-01@mail.ru>
2025-03-17 20:33:03 +0500

    init

---
+++ cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+func init() {
+	// init env
+	// init configs
+	// init db
+}
+
+func main() {
+	// init server
+	// middlewares (cors, swagger, logger, static, metrics)
+	// init router
+	// start server
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/spitfireooo/form-constructor-server-v2
+
+go 1.23.4
